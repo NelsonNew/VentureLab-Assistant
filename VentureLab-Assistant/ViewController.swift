a2b1584a8fdbe8a4afe6226117e350c73e792762
NelsonNew <n.neuhaeusel@gmail.com>
2021-06-06 19:16:40 +0200

    Initial Commit

---
+++ VentureLab-Assistant/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VentureLab-Assistant
+//
+//  Created by Venture Lab on 06.06.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
